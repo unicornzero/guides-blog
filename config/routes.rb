78b69678fcef65fe45993766a7081dc136629b68
unicornzero <rylee.k@gmail.com>
2013-07-01 15:51:37 -0600

    initial post setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 GuidesBlog::Application.routes.draw do
+  root to: "welcome#index"
+
+  resources :posts
+  
+  get "welcome/index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
