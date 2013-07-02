7fd85067757f4ca1ecf40504caec03eddcb28ae5
unicornzero <rylee.k@gmail.com>
2013-07-01 21:53:02 -0600

    expanded edit functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,18 +5,35 @@ class PostsController < ApplicationController
   end
 
   def new
+  	@post = Post.new
   end
 
   def create
   	@post = Post.new(post_params)
-  	@post.save
-  	redirect_to @post
+  	if @post.save
+  	  redirect_to @post
+  	else
+  		render 'new'
+  	end
   end
 
   def show
   	@post = Post.find(params[:id])
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(params[:post].permit(:title, :text))
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
 
   private
   	def post_params
